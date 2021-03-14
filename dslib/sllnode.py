235ab1bab4119eeca66c84750f8d6655bfdcd473
Roby Poteau <robypoteau@gmail.com>
2021-03-14 13:09:52 -0400

    Create singly linked list node stubs

---
+++ dslib/sllnode.py
@@ -0,0 +1,16 @@
+class SSLNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def get_data(self):
+        pass
+
+    def set_data(self, data):
+        pass
+
+    def get_next(self):
+        pass
+
+    def set_next(self, next):
+        pass
---
+++ tests/test_sllnode.py
@@ -0,0 +1,18 @@
+def test_constructor():
+    pass
+
+
+def test_get_data():
+    pass
+
+
+def test_set_data():
+    pass
+
+
+def test_get_next():
+    pass
+
+
+def test_set_next():
+    pass
