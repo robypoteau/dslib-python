4e0107f08df363ef790549aef398e5a5bbbd13ea
Roby Poteau <robypoteau@gmail.com>
2021-03-15 15:57:52 -0400

    Add desc and stubs for singly linked list class

---
+++ dslib/singlylinkedlist.py
@@ -0,0 +1,46 @@
+"""Create an object that enables you to construct a list of elements which are
+placed in nodes, in particularly the SLLNode class. Linked lists can grow
+dynamically without concern for elements being contiguous in memory. Each
+element points to the next element in the list.
+
+
+Functions:
+    * insert_node
+    * remove_node
+    * search
+    * size
+    * is_empty
+
+
+"""
+
+class LinkedList():
+    """
+    """
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        pass
+
+    def insert_node(self, data):
+        pass
+
+    def remove_node(self):
+        pass
+
+    def search(self, data):
+        pass
+
+    def size(self):
+        pass
+
+    def is_empty(self):
+        pass
---
+++ tests/test_singlylinkedlist.py
@@ -0,0 +1,32 @@
+from dslib.singlylinkedlist import LinkedList
+
+def test_init(self):
+    pass
+
+
+def test_repr(self):
+    pass
+
+
+def test_iterator(self):
+    pass
+
+
+def test_insert_node(self, data):
+    pass
+
+
+def test_remove_node(self):
+    pass
+
+
+def test_search(self):
+    pass
+
+
+def test_size(self):
+    pass
+
+
+def test_is_empty(self):
+    pass
