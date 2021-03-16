8bc6b0a7483ce420d33efc505def963f7687e437
Roby Poteau <robypoteau@gmail.com>
2021-03-16 03:56:22 -0400

    Add functionality and test to singly linked list

--- dslib/singlylinkedlist.py
+++ dslib/singlylinkedlist.py
@@ -1,3 +1,5 @@
+from dslib.sllnode import SLLNode
+
 """Create an object that enables you to construct a list of elements which are
 placed in nodes, in particularly the SLLNode class. Linked lists can grow
 dynamically without concern for elements being contiguous in memory. Each
@@ -10,37 +12,93 @@ Functions:
     * search
     * size
     * is_empty
-
-
 """
 
-class LinkedList():
-    """
-    """
 
+class LinkedList():
     def __init__(self):
-        pass
+        """Constructor for the LinkedList class.
+        """
+        self.head = None
+        self.length = 0
 
     def __repr__(self):
+        """
+        """
         pass
 
+    def __len__(self):
+        """Return the size of the linked list.
+
+        This operations runs in constant time, O(1).
+
+        Returns
+        -------
+        Integer
+            List size
+        """
+        return self.length
+
     def __iter__(self):
+        """
+        """
         pass
 
     def __next__(self):
+        """
+        """
         pass
 
     def insert_node(self, data):
-        pass
+        """Add a new node to the front of the linked list.
+
+        This operations runs in constant time, O(1).
+
+        Parameters
+        ----------
+        data : Object
+            Data is an object of any type
+        """
+        node = SLLNode(data)
+        node.set_next(self.head)
+        self.head = node
+        self.length += 1
 
     def remove_node(self):
-        pass
+        """Remove the node from the front of the linked list.
+
+        This operations runs in constant time, O(1).
+
+        Returns
+        -------
+        Object
+            Data in the node
+        """
+        if self.length == 0:
+            raise IndexError("no nodes in this linked list.")
+        data = self.head.get_data() if self.head else self.head
+        self.head = self.head.get_next() if self.head else self.head
+        self.length -= 1
+        return data
 
     def search(self, data):
+        """
+        """
         pass
 
     def size(self):
-        pass
+        """Return the size of the linked list.
+
+        This operations runs in constant time, O(1).
+
+        Returns
+        -------
+        Integer
+            List size
+        """
+        return self.length
 
     def is_empty(self):
+        """
+        """
         pass
--- tests/test_singlylinkedlist.py
+++ tests/test_singlylinkedlist.py
@@ -1,32 +1,77 @@
+import pytest
 from dslib.singlylinkedlist import LinkedList
 
-def test_init(self):
+
+def test_init():
+    ll = LinkedList()
+    assert ll is not None
+
+
+def test_repr():
     pass
 
 
-def test_repr(self):
+def test_iterator():
     pass
 
 
-def test_iterator(self):
+def test_insert_node():
+    data1 = "Datum"
+    ll = LinkedList()
+    ll.insert_node(data1)
+    assert ll.head.get_data() == data1
+
+    data2 = "Datum 2"
+    ll.insert_node(data2)
+    assert ll.head.get_data() == data2
+    assert ll.head.get_next().get_data() == data1
+
+    data3 = "Datum 3"
+    ll.insert_node(data3)
+    assert ll.head.get_data() == data3
+    assert ll.head.get_next().get_data() == data2
+    assert ll.head.get_next().get_next().get_data() == data1
+
+
+def test_remove_node():
+    data1 = "Datum"
+    ll = LinkedList()
+    ll.insert_node(data1)
+    data2 = "Datum 2"
+    ll.insert_node(data2)
+    data3 = "Datum 3"
+    ll.insert_node(data3)
+
+    assert ll.remove_node() == data3
+    assert ll.remove_node() == data2
+    assert ll.remove_node() == data1
+
+
+def test_search():
     pass
 
 
-def test_insert_node(self, data):
-    pass
+def test_size():
+    data1 = "Datum"
+    ll = LinkedList()
+    ll.insert_node(data1)
+    assert ll.size() == 1
+    data2 = "Datum 2"
+    ll.insert_node(data2)
+    assert ll.size() == 2
+    data3 = "Datum 3"
+    ll.insert_node(data3)
+    assert ll.size() == 3
+    ll.remove_node()
+    assert ll.size() == 2
+    ll.remove_node()
+    assert ll.size() == 1
+    ll.remove_node()
+    assert ll.size() == 0
+
+    with pytest.raises(IndexError):
+        ll.remove_node()
 
 
-def test_remove_node(self):
-    pass
-
-
-def test_search(self):
-    pass
-
-
-def test_size(self):
-    pass
-
-
-def test_is_empty(self):
+def test_is_empty():
     pass
