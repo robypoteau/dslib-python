0b1ae895ccbf0715dfb9fe4db6c19c5ea89a8b77
Roby Poteau <robypoteau@gmail.com>
2021-03-30 13:52:52 -0400

    Add queue files and dummy test

--- dslib/queue.py
+++ dslib/queue.py
@@ -1,5 +1,5 @@
 """Create of object where the last object to enter the data structure is
-the last object to be removed. Last IN Last OUT (FIFO).
+the last object to be removed. First IN First OUT (FIFO).
 
 Functions:
 
@@ -9,3 +9,91 @@ Functions:
     * size()
     * is_empty()
 """
+
+
+class Queue():
+    def __init__(self):
+        """Constructor for queue class.
+        """
+        self.__list = []
+
+    def __repr__(self):
+        """
+        """
+        return f"<{self.__class__.__name__} at {hex(id(self))}"
+
+    def __len__(self):
+        """Return the size of the queue.
+
+        This operations runs in constant time, O(1).
+
+        Returns
+        -------
+        Integer
+            List size
+        """
+        return len(self.__list)
+
+    def enqueue(self, data):
+        """Add an element to the back of a queue.
+
+        This operation runs in constant time, O(n).
+
+        Parameters
+        ----------
+        data : Object
+            Data is an object of any type
+        """
+        self.__list.insert(0, data)
+
+    def dequeue(self):
+        """Remove an element from the front of a queue.
+
+        This operations runs in linear time, O(1).
+
+        Returns
+        -------
+        data : Object
+            Data is an object of any type
+        """
+        if self.__list:
+            return self.__list.pop()
+        return None
+
+    def peek(self):
+        """Show the element at the front of the queue.
+
+        This operations runs in linear time, O(1).
+
+        Returns
+        -------
+        data : Object
+            Data is an object of any type
+        """
+        if self.__list:
+            return self.__list[-1]
+        return None
+
+    def size(self):
+        """Return the size of the queue.
+
+        This operations runs in constant time, O(1).
+
+        Returns
+        -------
+        Integer
+            List size
+        """
+        return len(self.__list)
+
+    def is_empty(self):
+        """Reveals whether the queue is empty.
+
+        This operations runs in constant time, O(1).
+
+        Returns
+        -------
+        Boolean
+            Returns True is the queue is empty
+        """
+        return len(self.__list) == 0
---
+++ tests/test_queue.py
@@ -0,0 +1,26 @@
+from dslib.queue import Queue
+
+
+def test_init():
+    queue = Queue()
+    assert queue is not None
+
+
+def test_enqueue():
+    pass
+
+
+def test_dequeue():
+    pass
+
+
+def test_peek():
+    pass
+
+
+def test_size():
+    pass
+
+
+def test_is_empty():
+    pass
