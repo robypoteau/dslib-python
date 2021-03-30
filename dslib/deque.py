c64e312f19586f21e0fbd346ab16f6fb613470f6
Roby Poteau <robypoteau@gmail.com>
2021-03-30 14:20:16 -0400

    Stub out deque class methods

--- dslib/deque.py
+++ dslib/deque.py
@@ -10,3 +10,68 @@ Functions:
     * size()
     * is_empty()
 """
+
+
+class Deque():
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        pass
+
+    def add_front(self, data):
+        """
+        """
+        pass
+
+    def remove_front(self):
+        """
+        """
+        pass
+
+    def peek_front(self):
+        """
+        """
+        pass
+
+    def add_rear(self, data):
+        """
+        """
+        pass
+
+    def remove_rear(self):
+        """
+        """
+        pass
+
+    def peek_rear(self):
+        """
+        """
+        pass
+
+    def size(self):
+        """Return the size of the linked list.
+
+        This operations runs in constant time, O(1).
+
+        Returns
+        -------
+        Integer
+            List size
+        """
+        pass
+
+    def is_empty(self):
+        """
+        """
+        pass
